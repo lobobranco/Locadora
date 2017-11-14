18f64416519263f9ec7813ecd454cb793162b733
Lobo Branco <lucasgaldino7@gmail.com>
2017-11-14 09:56:00 -0300

    Começando a entender

--- src/Filme.java
+++ src/Filme.java
@@ -6,6 +6,14 @@ public class Filme {
 	private String formato;
 	private String atores;
 	
+	public Filme(String titulo, String duracao, int ano, String formato, String atores) {
+		this.titulo = titulo;
+		this.duracao = duracao;
+		this.ano = ano;
+		this.formato = formato;
+		this.atores = atores;
+	}
+	
 	public String getTitulo() {
 		return titulo;
 	}
--- src/Sistema.java
+++ src/Sistema.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
 
 public class Sistema {
-
 	public static void main(String[] args) {
-		System.out.println("Apenas testando");
-	}
+		Scanner teclado = new Scanner(System.in);
+		int fimn;
+		
+		System.out.println("Bem vindo ao Sistema!");
+		do {
+		System.out.println("1 - Cadastrar Filme");
+		System.out.println("2 - Cadastrar Cópia");
+		System.out.println("3 - Cadastrar Sócio");
+		System.out.println("4 - Locação");
+		System.out.println("5 - Devolução");
+		System.out.println("6 - Sair");
+		System.out.println("Digite sua opção: ");
+		int opc = teclado.nextInt();
+		fimn=opc;
+		switch (opc) {
+		case 1: System.out.println("Teste Filme");
+		break;
+		case 2: System.out.println("Teste Copia");
+		break;
+		case 3: System.out.println("Teste Socio");
+		break;
+		case 4: System.out.println("Teste locação");
+		break;
+		case 5: System.out.println("Teste devolucao");
+		break;
+		case 6: System.out.println("Teste fim");
+		break;
+		default: System.out.println("Opção inválida");
+		break;
+		}
+	} while (fimn != 6); 
+		teclado.close();
+}
 }
